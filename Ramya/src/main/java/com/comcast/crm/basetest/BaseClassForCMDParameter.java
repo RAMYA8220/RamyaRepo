343ac1811bc0f5f200a8f44ff46f671fe00179f9
RAMYA8220 <guntojur4@gmail.com>
2024-06-29 14:55:33 +0530

    latest project

---
+++ Ramya/src/main/java/com/comcast/crm/basetest/BaseClassForCMDParameter.java
@@ -0,0 +1,110 @@
+package com.comcast.crm.basetest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.comcast.crm.generic.databaseutility.DataBaseUtility;
+import com.comcast.crm.generic.fileutility.ExcelUtility;
+import com.comcast.crm.generic.fileutility.FileUtility;
+import com.comcast.crm.generic.webdriverutility.JavaUtility;
+import com.comcast.crm.generic.webdriverutility.UtilityClassObject;
+import com.comcast.crm.generic.webdriverutility.WebDriverUtility;
+import com.comcast.crm.objectrepositoryutility.HomePage;
+import com.comcast.crm.objectrepositoryutility.LoginPage;
+
+public class BaseClassForCMDParameter {
+
+	
+	// -----------//create object//--------------
+	public DataBaseUtility dlib = new DataBaseUtility();
+	public FileUtility flib = new FileUtility();
+	public ExcelUtility elib = new ExcelUtility();
+	public WebDriverUtility wlib = new WebDriverUtility();
+	public JavaUtility jlib = new JavaUtility();
+
+	public WebDriver driver = null;
+	//public static WebDriver sdriver = null;
+
+	@BeforeSuite(groups = { "smokeTest", "regressionTest" })
+	public void configBS() throws Throwable {
+		System.out.println("====connect to db, report config===");
+		dlib.getDbconnection();
+	}
+	// @Parameters("BROWSER")
+	@BeforeClass(groups = { "smokeTest", "regressionTest" })
+
+	// public void configBc(String browser) throws Throwable //for crossbrowser
+	// parallel execution
+	public void configBc() throws Throwable {
+		System.out.println("==Launch the browser==");
+		
+		//String BROWSER = flib.getDataFromPropertiesFile("browser");
+		// passing data through cmd line
+		String BROWSER = System.getProperty("browser", flib.getDataFromPropertiesFile("browser") );
+
+		// String BROWSER=browser; //for crossbrowser parallel execution
+
+		if (BROWSER.equals("chrome")) {
+			driver = new ChromeDriver();
+		} else if (BROWSER.equals("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (BROWSER.equals("edge")) {
+			driver = new EdgeDriver();
+		} else {
+			driver = new ChromeDriver();
+		}
+		//sdriver=driver;
+		UtilityClassObject.setDriver(driver);
+	}
+
+	@BeforeMethod(groups = { "smokeTest", "regressionTest" })
+	public void configBM() throws Throwable {
+		System.out.println("===login to app==");
+		
+//		String URL = flib.getDataFromPropertiesFile("url");
+//		String USERNAME = flib.getDataFromPropertiesFile("username");
+//		String PASSWORD = flib.getDataFromPropertiesFile("password");
+		
+		
+		
+		//passing data through cmd line
+		String URL = System.getProperty("url", flib.getDataFromPropertiesFile("url"));
+		String USERNAME = System.getProperty("username",flib.getDataFromPropertiesFile("username"));
+		String PASSWORD = System.getProperty("password", flib.getDataFromPropertiesFile("password"));
+		LoginPage lp = new LoginPage(driver);
+		lp.loginToApp(URL, USERNAME, PASSWORD);
+
+	}
+
+	@AfterMethod(groups = { "smokeTest", "regressionTest" })
+	public void configAM() {
+		System.out.println("==logout an app==");
+		HomePage hp = new HomePage(driver);
+		hp.logout();
+	}
+
+	@AfterClass(groups = { "smokeTest", "regressionTest" })
+	public void configAc() {
+		System.out.println("===close the browser==");
+		driver.quit();
+	}
+
+	@AfterSuite(groups = { "smokeTest", "regressionTest" })
+	public void configAS() throws Throwable {
+		System.out.println("==close db, report backup==");
+		dlib.closeDbconnection();
+
+	}
+
+}
